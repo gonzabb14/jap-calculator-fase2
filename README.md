8b2987d670328dee45cb33c1ae356c178df69396
Gonzalo Maulella <83685692+gonzabb14@users.noreply.github.com>
2023-11-16 11:06:49 -0300

    save

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# jap-calculator-fase2
+# Score Calculator
+
+### How it works
+
+Calculate a score based on three exams with customized weights for each one.
+The first one, foundations, represents 30% of the total score, and the second, imperative, represents
+50% of the total score, and objects represent 20% of the total score.
+
+### Conditions to approve
+1. Have all English lessons approved, with an average of 50%.
+2. Have all CT lessons completed.
+3. Have at least 50% of the score
+
+
+|SCORE           |EVIDENCE                       |RESULT                       |
+|----------------|-------------------------------|-----------------------------|
+|60% or above    |`COMPLETED`                    | `APPROVED`                  |
+|50% and 50.99%  |`COMPLETED`                    | `SUPERVISED EXAM`           |
+|60% or above    |`UNCOMPLETED`                  | `SUPERVISED EXAM`           |
+
+
+> **Note:** Evidence is only a question that needs to be answered.
---
+++ assets/js/index.js
@@ -0,0 +1,134 @@
+let foundations = document.getElementById('foundations')
+let imperative = document.getElementById('imperative')
+let objects = document.getElementById('objects')
+
+function cleanErrorMessages() {
+    document.querySelectorAll(`.error-msg`).forEach(msg => msg.innerText = "")
+}
+
+
+
+document.getElementById('imperative').addEventListener('change', (evt) => {
+    calculate()
+});
+
+document.getElementById('foundations').addEventListener('change', () => {
+    calculate()
+});
+document.getElementById('objects').addEventListener('change', () => {
+    calculate()
+});
+
+['input', 'focusout'].forEach(event => {
+    document.getElementById('objects').addEventListener(event, (evt) => {
+        restrictValue(document.getElementById('objects'))
+        calculate()
+    });
+});
+
+['input', 'focusout'].forEach(event => {
+    document.getElementById('imperative').addEventListener(event, (evt) => {
+        restrictValue(document.getElementById('imperative'))
+        calculate()
+    });
+});
+
+['input', 'focusout'].forEach(event => {
+    document.getElementById('foundations').addEventListener(event, (evt) => {
+        restrictValue(document.getElementById('foundations'))
+        calculate()
+    });
+});
+
+
+function restrictValue(input) {
+    if (input === "") {
+        return;
+    }
+    if (input.value < 0) {
+        input.value = 0;
+    }
+    if (input.value > 100) {
+        input.value = 100;
+    }
+}
+
+function resetForm() {
+    cleanErrorMessages()
+
+    document.getElementById('imperative').value = ''
+    document.getElementById('objects').value = ''
+    document.getElementById('foundations').value = ''
+
+    document.getElementById('result').innerText = "0.00"
+
+    let color = hsl_col_perc(0, green, red);
+    setWavePercent(0)
+    setWaveColor(color)
+}
+
+function setWaveColor(color) {
+    document.getElementsByClassName('wave')[0].style.backgroundColor = color
+}
+
+function setWavePercent(percent) {
+    document.getElementsByClassName('wave')[0].style.top = (100 - percent).toString() + "%";
+}
+
+function setErrorMessage(field) {
+    if (document.getElementById(`${field.id}-msg-container`) !== null) {
+        document.getElementById(`${field.id}-msg-container`).innerText = field.title;
+    }
+}
+
+function validate() {
+    cleanErrorMessages()
+    let fields = document.querySelectorAll(':invalid')
+
+    fields.forEach(field => setErrorMessage(field))
+}
+
+function isNotANumber(value) {
+    return 'NaN' === value || 'Infinity' === value || undefined === value || 'undefined' === value || 'null' === value || null === value
+}
+
+// Just used as a shortcut for below, completely optional
+const red = 0,
+    yellow = 60,
+    green = 120,
+    turquoise = 180,
+    blue = 240,
+    pink = 300;
+function hsl_col_perc(percent, start, end) {
+    var a = percent / 100,
+        b = (end - start) * a,
+        c = b + start;
+
+    // Return a CSS HSL string
+    return 'hsl(' + c + ', 90%, 40%)';
+}
+
+function calculate() {
+    validate()
+
+    let imperative = document.getElementById('imperative').value || 0
+    let foundations = document.getElementById('foundations').value || 0
+    let objects = document.getElementById('objects').value || 0
+
+    let imperativeCalculated = parseFloat(imperative) * 0.30
+    let foundationsCalculated = parseFloat(foundations) * 0.20
+    let objectsCalculated = parseFloat(objects) * 0.20
+
+    let total = imperativeCalculated + foundationsCalculated + objectsCalculated
+
+    let color = hsl_col_perc((100 - total), green, red);
+    setWavePercent(total)
+    setWaveColor(color)
+
+    if (!isNotANumber(total.toString())) {
+        document.getElementById('result').innerText = total.toFixed(2)
+
+    } else {
+        document.getElementById('result').innerText = "0.00"
+    }
+}
